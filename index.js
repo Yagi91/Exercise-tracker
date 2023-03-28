7e3e41a7789951a33c561e3e6824a87625c88885
Yagi91 <bryantimah@gmail.com>
2023-03-28 01:21:27 +0100

    Install mongoose and bodypaser, add basic configuration and hello respond

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+// Basic Configuration
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Your first API endpoint
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "hello API" });
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
